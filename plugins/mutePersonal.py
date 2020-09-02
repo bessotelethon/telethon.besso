3fc5df40581afcf134708b2f5e62ee9e217543f4
bessotelethon <70536991+bessotelethon@users.noreply.github.com>
2020-09-03 01:00:58 +0300

    Add files via upload

--- plugins/mutePersonal.py
+++ plugins/mutePersonal.py
@@ -1,3 +1,19 @@
+import asyncio
+from utilities import utilities
+from Db.mute_sql import addMuteUser, getMutedUsers, getMutedUser, remMuteUser
+from telethon import utils, errors
+import re
+
+
+def escape(strin):
+    alphanumeric = ""
+    for character in strin:
+        if character.isalnum():
+            alphanumeric += character
+        else:
+            alphanumeric += "-"
+    return alphanumeric
+
 
 async def mute_user(message, from_id, chat_id, name):
     try:
@@ -10,6 +26,22 @@ async def mute_user(message, from_id, chat_id, name):
         return await message.reply("❏︙انته ليست مشرف")
     except errors.UserAdminInvalidError:
         return await message.reply("❏︙لا تستطيع كتم المشرفين")
+    except Exception as e:
+        utilities.prRed(str(type(e)) + " Error : " + str(e))
+        return await message.reply(str(e))
+
+
+async def unmute_user(message, from_id, chat_id):
+    try:
+        if not getMutedUser(chat_id, from_id):
+            return await message.reply("User already unmuted.")
+        await utilities.client.edit_permissions(chat_id, from_id, send_messages=True)
+        remMuteUser(chat_id, from_id)
+        return await message.reply("User unmuted successfully.")
+    except errors.ChatAdminRequiredError as e:
+        return await message.reply("Make me admin in group first.")
+    except errors.UserAdminInvalidError:
+        return await message.reply("Do not use it with admin dude.")
     except Exception as e:
         utilities.prRed(str(type(e)) + " Error : " + str(e))
         return await message.reply(str(e))
@@ -33,7 +65,7 @@ async def run(message, matches, chat_id, step, crons=None):
             return [mute_user(message, matches[1], chat_id, "muteById")]
         else:
             return [message.reply("please, use by reply or use valid username and id")]
-    elif matches[0] == "rmu":
+    elif matches[0] == "الغاء كتم":
         if re.match(r"@[a-zA-Z][\w\d]{3,30}[a-zA-Z\d]", matches[1]):
             user = await utilities.client.get_entity(matches[1])
             name = user.first_name
@@ -42,7 +74,7 @@ async def run(message, matches, chat_id, step, crons=None):
             return [unmute_user(message, matches[1], chat_id)]
         else:
             return [message.reply("please, use by reply or use valid username and id")]
-    elif matches == "كتم":
+    elif matches == "mu":
         if message.is_reply:
             msg = await message.get_reply_message()
             fromId = msg.from_id
@@ -50,7 +82,7 @@ async def run(message, matches, chat_id, step, crons=None):
             name = (await msg.get_sender()).first_name
             return [mute_user(message, fromId, chat_id, name)]
 
-    elif matches == "الغاء كتم":
+    elif matches == "rmu":
         if message.is_reply:
             msg = await message.get_reply_message()
             fromId = msg.from_id
@@ -73,5 +105,7 @@ plugin = {
         "^[!/#](getMuted)",
         "^[!/#](كتم)$",
         "^[!/#](الغاء كتم)$",
+        "^[!/#](mu) (.+)$",
+        "^[!/#](rmu) (.+)$",
     ],
 }
