06785cdef9b736e74227c94ecb41d2e8908b9fc6
bessotelethon <70536991+bessotelethon@users.noreply.github.com>
2020-09-04 20:26:36 +0300

    Add files via upload

--- plugins/lk.py
+++ plugins/lk.py
@@ -8,7 +8,7 @@ from userbot.utils import admin_cmd
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba, check pinned in @XtraTgBot"
 
-@command(outgoing=True, pattern="^/الاوامر$")
+@command(outgoing=True, pattern="^.الاوامر$")
 async def amireallyalive(alive):
 	
     """ For .alive command, check if the bot is running.  """
