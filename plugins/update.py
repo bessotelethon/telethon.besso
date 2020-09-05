3e64dc99a5c4f5f021aba288c637f18fb83a7010
bessotelethon <70536991+bessotelethon@users.noreply.github.com>
2020-09-05 13:52:52 +0300

    Add files via upload

---
+++ plugins/update.py
@@ -0,0 +1,43 @@
+import asyncio
+import subprocess
+import os
+import sys
+from utilities import utilities
+
+
+def runGitPull():
+    p = subprocess.Popen(
+        "git pull", stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True,
+    )
+    return iter(p.stdout.readline, b"")
+
+
+def restartBot():
+    p = subprocess.call("bash run.sh", shell=True)
+
+
+async def run(message, matches, chat_id, step, crons=None):
+    upd = ""
+    for line in runGitPull():
+        upd = upd + line.decode("utf-8")
+    if "Already" in upd:
+        return [message.reply("The source is up to date.")]
+    else:
+        utilities.config = utilities.get_config()
+        utilities.config["updateChat"] = message.chat_id
+        utilities.save_config()
+        await message.reply(
+            "The source has been updated,the bot will restart please wait."
+        )
+        restartBot()
+    return []
+
+
+plugin = {
+    "name": "update",
+    "desc": "Update the bot from source",
+    "usage": ["[!/#]update update bot from internal."],
+    "run": run,
+    "sudo": True,
+    "patterns": ["^[!/#]update$"],
+}
