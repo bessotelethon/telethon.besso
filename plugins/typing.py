4e305ee9f20bfa6e775df60f7e3df68b1cab7c6f
bessotelethon <70536991+bessotelethon@users.noreply.github.com>
2020-09-05 18:05:40 +0300

    Add files via upload

---
+++ plugins/typing.py
@@ -0,0 +1,35 @@
+import asyncio
+from utilities import utilities
+
+
+async def run(message, matches, chat_id, step, crons=None):
+    if not (message.out):
+        message = await message.reply(matches)
+    Responses = []
+    if matches != None:
+        text = matches.replace("\r", " ").replace("\n", " ").replace("\r\n", " ")
+        await message.edit("|")
+        msg_to_send = ""
+        for c in text:
+            clip = "ㅤ"
+            for i in range(0, 3):
+                await message.edit(msg_to_send + clip)
+                if clip == "|":
+                    clip = "ㅤ"
+                else:
+                    clip = "|"
+                await asyncio.sleep(0.1)
+            msg_to_send = msg_to_send + c
+            await message.edit(msg_to_send)
+            await asyncio.sleep(0.3)
+    return []
+
+
+plugin = {
+    "name": "",
+    "desc": "",
+    "usage": ["/يكتب + الاسم"],
+    "run": run,
+"sudo":False,
+    "patterns": ["^[!/#]يكتب (.*)$"],
+}
