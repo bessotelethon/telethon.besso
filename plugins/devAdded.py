965ec4b9203056cc8c2071c4b670b7901ab8a888
bessotelethon <70536991+bessotelethon@users.noreply.github.com>
2020-09-15 09:56:22 +0300

    Add files via upload

--- plugins/devAdded.py
+++ plugins/devAdded.py
@@ -11,7 +11,7 @@ async def addDev_user(message, from_id):
             return await message.reply("User already added as Dev.")
         addDevUser(from_id)
         utilities.devs.append(from_id)
-        return await message.reply("User is Dev now.")
+        return await message.reply("❏︙تم رفع مطور")
     except Exception as e:
         utilities.prRed(str(type(e)) + " Error : " + str(e))
         return await message.reply(str(e))
@@ -23,7 +23,7 @@ async def remDev_user(message, from_id):
             return await message.reply("User already not dev.")
         remDevUser(from_id)
         utilities.devs.remove(from_id)
-        return await message.reply("User is not dev now.")
+        return await message.reply("❏︙تم تنزيل مطور")
     except Exception as e:
         utilities.prRed(str(type(e)) + " Error : " + str(e))
         return await message.reply(str(e))
@@ -34,7 +34,7 @@ async def run(message, matches, chat_id, step, crons=None):
     response = []
     if message.sender_id not in utilities.config["sudo_members"]:
         return []
-    if matches == "getDevs":
+    if matches == "المطورين":
         devlist = getDevsUsers()
         res = ""
         i = 1
@@ -57,8 +57,8 @@ async def run(message, matches, chat_id, step, crons=None):
                 )
             i += 1
             res = res + strin
-        return [message.reply(res if (len(res) != 0) else "no devs")]
-    if matches[0] == "dev":
+        return [message.reply(res if (len(res) != 0) else "❏︙لا يوجد مطورين")]
+    if matches[0] == "رفع مطور":
         if re.match(r"@[a-zA-Z][\w\d]{3,30}[a-zA-Z\d]", matches[1]):
             user = await utilities.client.get_entity(matches[1])
             return [addDev_user(message, user.id)]
@@ -75,7 +75,7 @@ async def run(message, matches, chat_id, step, crons=None):
             return [remDev_user(message, matches[1])]
         else:
             return [message.reply("please, use by reply or use valid username and id")]
-    elif matches == "dev":
+    elif matches == "رفع مطور":
         if message.is_reply:
             msg = await message.get_reply_message()
             fromId = msg.from_id
@@ -89,12 +89,12 @@ async def run(message, matches, chat_id, step, crons=None):
             fromId = msg.from_id
             chat_id = msg.chat_id
             return [remDev_user(message, fromId)]
-    elif matches == "rdevall":
+    elif matches == "مسح المطورين":
         devlist = getDevsUsers()
         for user in devlist:
             remDevUser(user.user_id)
             utilities.devs.remove(user.user_id)
-        return [message.reply("done.")]
+        return [message.reply("❏︙تم تنزيل جميع المطورين")]
     return response
 
 
@@ -102,21 +102,19 @@ plugin = {
     "name": "",
     "desc": "Make someone dev",
     "usage": [
-        "[!/#]rdevall delete all devs that has been promoted.",
-        "[!/#]getDevs get all devs users in bot.",
-        "[!/#]dev in reply to message to dev a user.",
-        "[!/#]rdev in reply to message to undev a user.",
-        "[!/#]dev <id or username> to dev a user by id/username.",
-        "[!/#]rdev <id or username> to undev a user by id/username.",
+        "/مسح المطورين",
+        "/المطورين",
+        "/تنزيل مطور + برد",
+        "/رفع مطور + برد",
     ],
     "run": run,
     "sudo": True,
     "patterns": [
-        "^[!/#](rdevall)$",
-        "^[!/#](getDevs)",
+        "^[!/#](مسح المطورين)$",
+        "^[!/#](المطورين)",
         "^[!/#](dev)$",
-        "^[!/#](rdev)$",
-        "^[!/#](dev) (.+)$",
+        "^[!/#](تنزيل مطور)$",
+        "^[!/#](رفع مطور) (.+)$",
         "^[!/#](rdev) (.+)$",
     ],
 }
